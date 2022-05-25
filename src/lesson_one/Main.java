77d874f2d416a572daaa1cb63dd31dc273a63aee
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-25 15:20:43 +0600

    Edit task3

--- src/lesson_one/Main.java
+++ src/lesson_one/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(exponentiationTwo(2, 4));
         System.out.println(exponentiation(0, 200));
         System.out.println(exponentiation(50, 0));
+        System.out.println(sumFromTo(0,100));
     }
 
     public static long exponentiation(long number, int stage) {
@@ -29,5 +30,9 @@ public class Main {
             sum = sum * number;
         }
         return sum;
+    }
+
+    public static double sumFromTo(int from, int to) {
+        return to*(to+1.0)/2;
     }
 }

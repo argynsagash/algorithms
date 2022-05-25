9acb776af2392f16a4c4574c50ae10b3e6380db3
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-25 14:21:19 +0600

    Do check

--- src/lesson_one/Main.java
+++ src/lesson_one/Main.java
@@ -2,6 +2,7 @@ package lesson_one;
 
 public class Main {
     public static void main(String[] args) {
+        //check
         System.out.println(exponentiation(2,6));
         System.out.println(exponentiation(0,200));
         System.out.println(exponentiation(50,0));

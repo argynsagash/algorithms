809feeaef6886bd25ab23ca06550c5d68017ad22
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-25 14:51:33 +0600

    Edit images

--- src/lesson_one/Main.java
+++ src/lesson_one/Main.java
@@ -3,7 +3,7 @@ package lesson_one;
 public class Main {
     public static void main(String[] args) {
         //check
-        System.out.println(exponentiationTwo(2, 8));
+        System.out.println(exponentiationTwo(2, 4));
         System.out.println(exponentiation(0, 200));
         System.out.println(exponentiation(50, 0));
     }
@@ -21,11 +21,10 @@ public class Main {
     public static long exponentiationTwo(long number, int stage) {
         if (stage == 0) return 1;
         if (number == 0) return 0;
-
-        long sum = number;
         if (stage % 2 == 0) {
             exponentiationTwo(number * number, stage / 2);
         }
+        long sum = number;
         for (int i = 1; i < stage; i++) {
             sum = sum * number;
         }

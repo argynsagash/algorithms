938c1e3b57104ea119ab6c61f3be828df9e1d5d9
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-25 12:38:51 +0600

    Initial commit

---
+++ src/lesson_one/Main.java
@@ -0,0 +1,19 @@
+package lesson_one;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(exponentiation(2,6));
+        System.out.println(exponentiation(0,200));
+        System.out.println(exponentiation(50,0));
+    }
+
+    public static long exponentiation(long number, int stage) {
+        if (stage == 0) return 1;
+        if (number == 0) return 0;
+        long sum = number;
+        for (int i = 1; i < stage; i++) {
+            sum = sum*number;
+        }
+        return sum;
+    }
+}
